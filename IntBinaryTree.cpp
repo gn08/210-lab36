b24e7784e637d5a5edb20c691f8e4306c0e4f2aa
gn08 <gabbyfilipova@gmail.com>
2024-11-30 13:06:38 -0800

    commit 1:05

--- IntBinaryTree.cpp
+++ IntBinaryTree.cpp
@@ -11,26 +11,22 @@ IntBinaryTree::IntBinaryTree() {
 // The function inserts the node into the tree pointed to by 
 // the TreeNode pointer. This function is called recursively.
 void IntBinaryTree::insert(TreeNode *&nodePtr, TreeNode *&newNode) {
-   if (!nodePtr)
-      nodePtr = newNode;                  // Insert the node.
-   else if (newNode->value < nodePtr->value)
-      insert(nodePtr->left, newNode);     // Search the left branch
-   else 
-      insert(nodePtr->right, newNode);    // Search the right branch
+   if (nodePtr == nullptr){
+      nodePtr = new TreeNode;                  // Insert the node.
+      nodePtr->value = value;
+      nodePtr->left = nullptr;
+      nodePtr->right = nullptr;
+   } else if (value < nodePtr->value){
+      insert(nodePtr->left, value);     // Search the left branch
+   } else {
+      insert(nodePtr->right, value);    // Search the right branch
+   }
 }
 
 // insertNode creates a new node to hold num as its value,
 // and passes it to the insert function.                  
 void IntBinaryTree::insertNode(int num) {
-   TreeNode *newNode;      // Pointer to a new node.
-
-   // Create a new node and store num in it.
-   newNode = new TreeNode;
-   newNode->value = num;
-   newNode->left = newNode->right = nullptr;
-   
-   // Insert the node.
-   insert(root, newNode);
+   insert(root, value);
 }
 
 // destroySubTree is called by the destructor. It
