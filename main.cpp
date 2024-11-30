750d60fe4adf661e2ce88bd835ef607a375b5a96
gn08 <gabbyfilipova@gmail.com>
2024-11-30 13:29:51 -0800

    commit 1:30

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 #include "IntBinaryTree.h"
 using namespace std;
 
-int main() {
-    
+void displayMenu(){
+    cout << endl << "Menu: " << endl;
+    cout << "1. Insert code " << endl;
+    cout << "2. Display codes in order " << endl;
+    cout << "3. Exit " << endl;
+}
 
+int main() {
+    IntBinaryTree tree;
+    int choice;
+    string code;
+
+    do{
+        
+    }
     return 0;
 }
