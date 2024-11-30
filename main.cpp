1176bd72111731858900f9134bd78621cb7ba21f
gn08 <gabbyfilipova@gmail.com>
2024-11-30 13:51:14 -0800

    commit 1:50

--- main.cpp
+++ main.cpp
@@ -15,14 +15,24 @@ int main() {
     string code;
 
     do{
+        displayMenu();
+        cin >> choice;
+        cin.ignore();
 
         switch(choice){
             case 1:
-                //
+                cout << "Enter code: ";
+                getline(cin, code);
+                tree.insertNode(code);
+                cout << "Code inserted" << endl;
+                break;
             case 2:
-                //
+                cout << "Codes in order: ";
+                tree.displayInOrder();
+                break;
             case 3:
-                //
+                cout << "Exiting" << endl;
+                break;
             default:
                 cout << "Try again" << endl;
         }
